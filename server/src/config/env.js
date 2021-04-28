ec9a1907a1d831717f6aa6f2697cfdd439fb485a
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-04-28 15:22:46 -0500

    Added googleapis, cloudinary credentials and ignore public folder.

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -7,6 +7,12 @@ module.exports = {
     clientSecret: process.env.CLIENT_SECRET,
     resfreshToken: process.env.REFRESH_TOKEN,
   },
+  cloudinaryCredentials: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    apiSecret: process.env.CLOUDINARY_API_SECRET,
+    folder: process.env.CLOUDINARY_FOLDER,
+  },
   jwtSecret: process.env.JWT_SECRET,
   mongodbURL: process.env.MONGODB_URL,
 };
