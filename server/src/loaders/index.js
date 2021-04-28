d3fc27be451a92645d982c7cbf6c181a54e8145c
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-04-28 15:20:15 -0500

    Subscribers loader added.

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,9 +1,11 @@
 const expressLoader = require("./express");
 const mongooseLoader = require("./mongoose");
+const { loadSubscribers } = require("../subscribers");
 
 module.exports = async (app) => {
   console.log("Init server loaders...");
 
   await mongooseLoader();
   expressLoader(app);
+  loadSubscribers();
 };
