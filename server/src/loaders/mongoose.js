87b514b9d965280bd993b06e29fc9d2251fa42e1
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-04-28 18:27:56 -0500

    Try/Catch block added to handle db connections errors.

--- server/src/loaders/mongoose.js
+++ server/src/loaders/mongoose.js
@@ -4,21 +4,25 @@ const { mongodbURL } = require("../config/env");
 module.exports = async () => {
   const connection = mongoose.connection;
 
-  connection.on("open", () => {
-    console.log("Database is connected");
-  });
+  try {
+    connection.on("open", () => {
+      console.log("Database is connected");
+    });
 
-  connection.on("error", (error) => {
+    connection.on("reconnected", () => {
+      console.log("Database has been reconected");
+    });
+
+    connection.on("disconnected", () => {
+      console.log("Database disconnected");
+    });
+
+    await mongoose.connect(mongodbURL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
     console.log(error);
-  });
-
-  connection.on("disconnected", () => {
-    console.log("Database disconnected");
-  });
-
-  await mongoose.connect(mongodbURL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  }
 };
